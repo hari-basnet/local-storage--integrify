49e0fdbdc44283126eaaebd57e16887efd3d1be7
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-04-03 22:53:00 +0300

    added create contend function

--- js/script.js
+++ js/script.js
@@ -1,47 +1,58 @@
 // selectors 
-const itemDescription = document.querySelector('.description');
-const itemAmount = document.querySelector('.amount');
+const itemDescription = document.querySelector('#description');
+const itemAmount = document.querySelector('#amount');
 const incomeBox = document.querySelector('.income-box');
 const select = document.querySelector('#selection');
+const incomeData = document.querySelector('.income-data');
+const expenseData = document.querySelector('.expense-data');
+const addButton = document.querySelector('.btn-add');
+const incomeArray = []
+const expenseArray = []
+let isValidated = false;
 
 
-function checkSelection (){
+function checkSelection() {
+
+}
+
+function createContent() {
+
+
     let selectedValue = select.options[select.selectedIndex].value;
     console.log(select.options);
-    
-    if(selectedValue == 'income'){
-        console.log('income');
+
+    if (selectedValue == 'income') {
+        incomeData.innerHTML += `<div>
+        <span>${itemDescription.value}</span>
+        <span>${itemAmount.value}€</span>
+        </div>`;
+        // incomeArray.push(parseInt(itemAmount.value));
     }
-    
-    else if(selectedValue == 'expense'){
+
+    else if (selectedValue == 'expense') {
+        expenseData.innerHTML += `<div>
+        <span>${itemDescription.value}</span>
+        <span>${itemAmount.value}€</span>
+        </div>`;
         console.log('expense');
     }
-    else{
+    else {
         console.log('select either income or expense');
-        
+
     }
 }
 
-checkSelection();
-function createContent (){
-
-    return ``
-    
-
-}
-
-function checkBalance () {
+function checkBalance() {
 
 
 }
+
 
 
 // action listener 
-
-// if (income is selected){
-
-// }
-
-// if(expense is selected ){
-    
-// }
+addButton.addEventListener('click', function (e){
+    isValidated = true;
+    if(isValidated){
+        createContent();
+    }
+});
