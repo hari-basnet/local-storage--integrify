9fbcad6156abb0c3bc9c48078ff50b57ae91f765
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-04-03 17:49:56 +0300

    new lines of code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
 // selectors 
 const itemDescription = document.querySelector('.description');
 const itemAmount = document.querySelector('.amount');
+const incomeBox = document.querySelector('.income-box');
+const select = document.querySelector('#selection');
 
 
+function checkSelection (){
+    let selectedValue = select.options[select.selectedIndex].value;
+    console.log(select.options);
+    
+    if(selectedValue == 'income'){
+        console.log('income');
+    }
+    
+    else if(selectedValue == 'expense'){
+        console.log('expense');
+    }
+    else{
+        console.log('select either income or expense');
+        
+    }
+}
 
+checkSelection();
 function createContent (){
 
+    return ``
     
 
 }
 
 function checkBalance () {
+
 
 }
 
