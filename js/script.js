24da9cf1dc5465b7720cda92e85b7a4013e77cc1
Hari Basnet <harikrishna.basnet@yahoo.com>
2019-04-03 15:38:30 +0300

    css added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// selectors 
+const itemDescription = document.querySelector('.description');
+const itemAmount = document.querySelector('.amount');
+
+
+
+function createContent (){
+
+    
+
+}
+
+function checkBalance () {
+
+}
+
+
+// action listener 
+
+// if (income is selected){
+
+// }
+
+// if(expense is selected ){
+    
+// }
